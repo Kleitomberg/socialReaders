8c56bf7b0236b001203cc5e0ba5354d41237728a
kleitomberg <kleitom-1@hotmail.com>
2022-08-03 19:50:33 -0300

    implelmentando home

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController{
+
+    /**
+     *
+     *
+     * @Route("/")
+     */
+    public function Indexpage(){
+
+        return $this->render("index.html.twig",[
+
+        ]);
+    }
+
+
+}
