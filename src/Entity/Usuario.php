cdff7df42453bd60646fd29cda496191271db426
kleitomberg <kleitom-1@hotmail.com>
2022-08-04 17:38:32 -0300

    login de usuario, bloqueando rota

--- src/Entity/Usuario.php
+++ src/Entity/Usuario.php
@@ -2,9 +2,11 @@
 
 namespace App\Entity;
 
+
 use App\Repository\UsuarioRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: UsuarioRepository::class)]
