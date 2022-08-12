e0475ce1c2d19ff8a97001f5fc37f02cdeb2fe9e
kleitomberg <kleitom-1@hotmail.com>
2022-08-12 15:33:04 -0300

    serach frineds

--- src/Controller/AmigosController.php
+++ src/Controller/AmigosController.php
@@ -11,6 +11,7 @@ use App\Entity\SolicitacaoAmizade;
 use App\Repository\SolicitacaoAmizadeRepository;
 
 use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\Response;
 
 class AmigosController extends AbstractController{
 
@@ -30,37 +31,102 @@ class AmigosController extends AbstractController{
 
 
             dump($id_solicitante_strng);
+
+            if($id_solicitante==$id_solicitante_strng){
+
             $amigos = $solicitacaoAmizadeRepository->findBy(
 
-                array('id_solicitante' => $id_solicitante_strng, 'situacao' => 1,
+                array('id_solicitante' => $id_solicitante_strng, 'situacao' => 1,)
+
+            );
+
+        }else{
+
+            $amigos = $solicitacaoAmizadeRepository->findBy(
+
+                array('id_solicitado' => $id_solicitante_strng, 'situacao' => 1,)
+
+            );
+        }
+
+
+            $array = [];
+            $arrayFriends = [];
+            foreach ($amigos as &$amigo) {
+               $friend = array($amigo->getIdSolicitado()->getId());
+              // array_push($array, $friend);
+               //dump($array);
+
+
+
+
+            $meusamigos = $userRepository->findBy(
+
+                array('id' => $friend,
 
                 )
 
             );
 
-            foreach ($amigos as &$amigo) {
-                $amigo->getIdSolicitado()->getId();
-                dump($amigo);
-
-                $meusamigos = $userRepository->findBy(
-
-                    array('id' => $amigo,
-
-                    )
-
-                );
+            array_push($arrayFriends, $meusamigos);
 
 
-            }
+        }
+
+        $arrayAmigos=[];
+        foreach ($arrayFriends as &$af) {
+           // dump($af[0]);
+            array_push($arrayAmigos, $af[0]);
+
+        }
+        dump($arrayAmigos);
+
+        //$arrayobjeto = $arrayFriends;
+        //dump($arrayobjeto);
+
+        return $this->render("amigos/listar.html.twig",[
+            "myfriends" => $arrayAmigos
+        ]);
+    }
 
 
 
+    /**
+         * @Route("/buscaramigos", name="app_serachamigos")
+         */
 
-            //dump($amigos[0]->getIdSolicitado()->getId());
-            dump($meusamigos);
+
+    public function buscarAmigos(SolicitacaoAmizadeRepository $solicitacaoAmizadeRepository, UserRepository $userRepository){
 
 
-        return $this->render("amigos/listar.html.twig");
+
+        if ( isset( $_POST["friends"] ))
+        {
+            $amigobusca = $_POST['friends'];
+
+            dump($amigobusca);
+
+            $serachfriend = $userRepository->findBy(
+
+                array('nome' => (string)$amigobusca,
+
+                )
+
+            );
+            dump($serachfriend);
+
+            return $this->render("amigos/listar.html.twig",[
+                'frindssearch'=>$serachfriend,
+                'myfriends'=>''
+
+            ]);
+
+        }
+
+        return new Response('Erro');
+       // return $this->redirectToRoute("app_serachamigos",);
+
+
     }
 
 
